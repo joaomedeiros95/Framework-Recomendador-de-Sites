bd60f90e50f004c0cac684831b9e532d579a7141
joaomedeiros95 <joaoribeiromedeiros@gmail.com>
2014-11-06 21:28:48 -0300

    Primeira versão

---
+++ src/com/ufrn/recomendador/sites/framework/Programa.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.ufrn.recomendador.sites.framework;
+
+/**
+ * @author joaomedeiros95
+ *
+ */
+public class Programa {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
